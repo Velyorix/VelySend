754bf29b7965d7e6f01b28ecc431aaaa0c9ef42f
YuketsuSh <sunshineoff1337@gmail.com>
2025-09-26 15:15:01 +0200

    feat(models): réexport des modèles (account, template, contact, email_job)

--- src-tauri/src/models/mod.rs
+++ src-tauri/src/models/mod.rs
@@ -2,3 +2,8 @@ pub mod account;
 pub mod template;
 pub mod contact;
 pub mod email_job;
+
+pub use account::Account;
+pub use template::Template;
+pub use contact::Contact;
+pub use email_job::EmailJob;
